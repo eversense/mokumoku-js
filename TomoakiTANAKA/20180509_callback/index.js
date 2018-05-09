e3b69058ba04a832ee141baff30aafaef49eb4e2
TomoakiTANAKA <tanaka1987@gmail.com>
2018-05-09 12:49:32 +0900

    いらないの消す

--- TomoakiTANAKA/20180509_callback/index.js
+++ TomoakiTANAKA/20180509_callback/index.js
@@ -17,38 +17,3 @@ console.log(11);
 console.log(12);
 console.log(13);
 console.log(14);
-
-
-// undefined
-var person;
-typeof(person);
-
-// object
-var person = {};
-typeof(person);
-
-// function
-function Person() {}
-typeof(Person);
-
-var person = new Person();
-typeof(person);
-
-
-//proto.prototypeは自動で生成
-// オブジェクトの定義
-function Employee() {}
-Employee.prototype.work = function() {
-  console.log("会社のいぬです");
-}
-
-var employee = new Employee();
-employee.work(); // 会社のいぬです
-
-function Engineer() {
-  this.work = function() {
-    console.log('わんわん');
-  };
-}
-var shun = new Engineer();
-shun.work(); //'わんわん';
