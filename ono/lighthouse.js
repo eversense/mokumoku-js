ff37fa773d6b32b0382b0624444d27a610001e09
Shunsuke Ono <shunsukeono.dev@gmail.com>
2018-06-06 13:04:02 +0900

    Update lighthouse.js

--- ono/lighthouse.js
+++ ono/lighthouse.js
@@ -25,12 +25,13 @@ module.exports.audit = (event, context, callback, chrome) => {
   let messages = [];
 
   // ループで複数URLに対してLighthouse関数実行する
-
+ 
   // lighthouse関数は並列の非同期処理ができない
   // map/for等のループで実行すると、前の処理を待ってくれないので、async/awaitを使って連続した非同期処理を実装
   async function runLighthouse() {
     for (let url of urls) {
-      // lighthouse関数のawait 
+      // lighthouse関数のawait
+      // lighthouse関数はpromiseを返す see: https://github.com/GoogleChrome/lighthouse/blob/9823a6579e59b8ea86620bf3dec59a7fd66110c4/lighthouse-core/runner.js
       // Promiseの結果が返されるまで、async function内の処理(forループ)を一時停止する
       await lighthouse(url, opts, perfConfig).then(results => {
         delete results.artifacts;
